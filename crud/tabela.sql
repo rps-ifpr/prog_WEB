1919feb89fe4fdcaa15c55fc71450286f94765a8
Rogério Santos <rogerio.dosantos@ifpr.edu.br>
2025-03-25 15:49:59 -0300

    Update tabela.sql

--- crud/tabela.sql
+++ crud/tabela.sql
@@ -10,6 +10,8 @@
 /*
  * Tabela criada dentro do database: db_crud_teste
  */
+ 
+ CREATE DATABASE db_crud_teste;
 CREATE TABLE `cliente` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `nome` varchar(30) DEFAULT NULL,
